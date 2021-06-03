c99f4572529cc94eba39f2ff2467b5ed2ffc47c7
Shoshana Newman <sho.newman@gmail.com>
2021-06-03 13:50:14 -0700

    Fix image opacity on image click function
    Co-authored-by: Jenn <jennifer.bordon@gmail.com>

--- animal-wiki/js/scripts.js
+++ animal-wiki/js/scripts.js
@@ -4,25 +4,25 @@ $(document).ready(function() {
     $('#octopusPic, #hamsterPic, #peacockPic').removeClass('selected');
     $('#monkey').show();
     $('#octopus, #hamster, #peacock').hide();
-    $('#octopusPic, #hamsterPic, #peacockPic').addClass('selected');
+    $('#monkeyPic').addClass('selected');
   })
   $('#octopusPic').click(function() {
     $('#monkeyPic, #hamsterPic, #peacockPic').removeClass('selected');
     $('#octopus').show();
     $('#monkey, #hamster, #peacock').hide();
-    $('#monkeyPic, #hamsterPic, #peacockPic').addClass('selected');
+    $('#octopusPic').addClass('selected');
   }) 
   $('#hamsterPic').click(function() {
     $('#monkeyPic, #octopusPic, #peacockPic').removeClass('selected');
     $('#hamster').show();
     $('#monkey, #octopus, #peacock').hide();
-    $('#monkeyPic, #octopusPic, #peacockPic').addClass('selected');
+    $('#hamsterPic').addClass('selected');
   })
   $('#peacockPic').click(function() {
     $('#monkeyPic, #hamsterPic, #octopusPic').removeClass('selected');
     $('#peacock').show();
     $('#monkey, #hamster, #octopus').hide();
-    $('#monkeyPic, #hamsterPic, #octopusPic').addClass('selected');
+    $('#peacockPic').addClass('selected');
   })
 
 // Select from dropdown menu for information  
