60f385860b9f0dbcea90df73ef71e4617ba0d82f
Shoshana Newman <sho.newman@gmail.com>
2021-06-03 16:50:41 -0700

    Add jquery and js to make forms function and add style form elements and results div
    Co-authored-by: Jenn <jennifer.bordon@gmail.com>

--- celebrity-dating/js/scripts.js
+++ celebrity-dating/js/scripts.js
@@ -15,6 +15,29 @@ function tally(checked) {
 
 //UI logic
 $(document).ready(function() {
-  const firstAnswer = $('input:radio[name=firstQuestion]:checked').val();
-  const firstAnswer = $('input:radio[name=firstQuestion]:checked').val();
+  $('form#survey').submit(function(e) {
+    e.preventDefault();
+    const firstAnswer = $('input:radio[name=firstQuestion]:checked').val();
+    const secondAnswer = $('input:radio[name=secondQuestion]:checked').val();
+    const thirdAnswer = $('input:radio[name=thirdQuestion]:checked').val();
+    tally(firstAnswer);
+    tally(secondAnswer);
+    tally(thirdAnswer);
+
+
+    if (daniel > gollum && daniel > keanu) {
+      $('#gollumSelected').hide();
+      $('#keanuSelected').hide();
+      $('#danielSelected').fadeIn();
+    } else if (gollum > daniel && gollum > keanu) {
+      $('#keanuSelected').hide();
+      $('#danielSelected').hide();
+      $('#gollumSelected').fadeIn();
+    } else if (keanu > daniel && keanu > gollum) {
+      $('#gollumSelected').hide();
+      $('#danielSelected').hide();
+      $('#keanuSelected').fadeIn();
+    }
+
+  });
 })
